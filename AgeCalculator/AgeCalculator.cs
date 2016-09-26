854d229873c8484a9483368b10f881baf0abb206
mathijsvds <mjn.vandersangen@student.avans.nl>
2016-09-26 17:06:39 +0200

    Opdracht is af

---
+++ AgeCalculator/AgeCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using System.Globalization;
+
+namespace AgeCalculatorNamespace
+{
+    public class AgeCalculator : IAgeCalculator
+    {
+        public bool ParseInput(string input, out DateTime result)
+        {
+            // validate input, both for single and double digits
+            if (DateTime.TryParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
+            {
+                return true;
+            }
+            else if (DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public int CalculateAge(DateTime dateOfBirth)
+        {
+            int years = DateTime.Today.Year - dateOfBirth.Year;
+            // if birthday has not yet passed this year, substract a year
+            if (DateTime.Today.DayOfYear < dateOfBirth.DayOfYear)
+            {
+                years--;
+            }
+
+            return years;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgeCalculatorNamespace;
 
 namespace Calculator
 {
@@ -10,8 +11,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Success!");
-            Console.ReadLine();
+            AgeCalculator calc = new AgeCalculator();
+            Console.WriteLine("Geef een datum om te berekenen (format: dd-mm-yyyy):");
+            Console.WriteLine("Typ 'Q' om te stoppen\n");
+            String input =  Console.ReadLine();
+
+            while (input.ToLower() != "q")
+            {
+                DateTime result;
+                if (calc.ParseInput(input, out result))
+                {
+                    Console.WriteLine("Je leeftijd is {0}", calc.CalculateAge(result));
+                    Console.WriteLine("\nGeef een datum om te berekenen (format: dd-mm-yyyy):");
+                }
+                else
+                {
+                    Console.WriteLine("Ongeldige input! probeer het nogmaals (dd-mm-yyyy):");
+                }
+                
+                input = Console.ReadLine();
+            }
         }
     }
 }
